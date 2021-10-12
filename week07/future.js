46e067dd8c7736026afd6ee7e5a411efe468f581
ArmendarizHub <89496858+ArmendarizHub@users.noreply.github.com>
2021-10-12 16:59:18 -0500

    Create future.js

---
+++ week07/future.js
@@ -0,0 +1,12 @@
+var job = ["U.S Army Soldier", "Programmer", "Business person", "Rockstar"];
+var city = ["Dallas", "Austin", "Miami", "New York", "Los Angeles"];
+var spouse = ["Elizabeth", "Liz", "Eliza"]
+var numKids = [0, 1, 2, 3, 4];
+
+var xx = Math.floor(Math.random() * 4)
+
+function fortune(job, city, spouse, numKids) {
+document.write("You will be a " + job + " in " + city + ", and married to " + spouse + " with " + numKids + " kids.");
+}
+
+fortune (job[xx], city[xx], spouse[xx], numKids[xx])
